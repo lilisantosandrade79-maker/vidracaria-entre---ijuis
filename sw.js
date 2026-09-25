2b578e6eaba4bb2faca8b73bb7b4453b8d5ff277
lilisantosandrade79-maker <lilisantosandrade79@gmail.com>
2026-09-25 17:26:15 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE = 'vei-pwa-v2';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('message', event => {
+  if (event.data?.type === 'SKIP_WAITING') self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  const url = new URL(req.url);
+  if (url.origin !== location.origin) return;
+
+  event.respondWith(
+    fetch(req).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(cache => cache.put(req, clone)).catch(() => {});
+      return res;
+    }).catch(() => caches.match(req).then(cached => cached || caches.match('./index.html')))
+  );
+});
